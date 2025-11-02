8b0005589911ec44f8e951b7e48aae73301c204f
Hariharan S <hh6236391@gmail.com>
2025-11-02 18:04:55 +0530

    fix: move package.json to root

---
+++ live-camera-viewer/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lifs.cameraapp',
+  appName: 'live-camera-viewer',
+  webDir: 'build'
+};
+
+export default config;
