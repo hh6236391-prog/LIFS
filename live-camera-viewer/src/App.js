dab1d57bc0e762fcd3618d95a351aed5ac6c0dc1
Hariharan S <hh6236391@gmail.com>
2025-11-02 10:48:51 +0530

    Initial commit after removing nested repo

---
+++ live-camera-viewer/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LiveImageViewer from './LiveImageViewer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LiveImageViewer />
+    </div>
+  );
+}
+
+export default App;
